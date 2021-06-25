fcfcae8d3b90f6c0070af513657dc6c4a5028d8b
Ibrahim Mostafa <>
2021-06-25 14:27:00 +0200

    intial commit

---
+++ C3SInterviewTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C3SInterviewTask
+//
+//  Created by Ibrahim on 25/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
